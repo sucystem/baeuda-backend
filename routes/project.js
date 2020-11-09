e7926d95f6fb8a5a6fecd4d5e7b26d61f7e469fe
CultureAddiction <didqhtmd@gmail.com>
2020-11-09 19:57:22 +0900

    project.js 수정 적용?

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.send('respond of project request');
-});
+  // project와 study 내가 해보기.
 
+});
+router.get('/write', function(req, res, next){
+    res.render('write', {title: "게시판 글 쓰기"});
+});
 module.exports = router;
