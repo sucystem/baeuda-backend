20c50d680a29dd0458dfae99d57ad896a5def042
sucystem <sucystem@outlook.com>
2020-11-30 00:23:15 +0900

    login debug

--- sql/lecture.js
+++ sql/lecture.js
@@ -1,5 +1,5 @@
 module.exports = {
-    selectLectureById = "SELECT * FROM baeuda.lecture WHERE id=?",
-    selectLecturesByUserId = "SELECT * FROM baeuda.lecture as l join lecture_student as s on l.id = s.lecture_id where student_id=?",
-    checkProfForLecture = "SELECT * FROM baeuda.lecture WHERE id=? AND prof=?"
-}
+    selectLectureById: "SELECT * FROM baeuda.lecture WHERE id=?",
+    selectLecturesByUserId: "SELECT * FROM baeuda.lecture as l join lecture_student as s on l.id = s.lecture_id where student_id=?",
+    checkProfForLecture: "SELECT * FROM baeuda.lecture WHERE id=? AND prof=?"
+};
