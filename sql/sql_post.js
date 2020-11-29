64669d4bf7a9677dfb63191d8cab320947bc81b9
CultureAddiction <didqhtmd@gmail.com>
2020-11-30 02:40:26 +0900

    beforeChange:showingPostMethod

--- sql/sql_post.js
+++ sql/sql_post.js
@@ -2,5 +2,7 @@ module.exports = {
     selectPostByUserId: "SELECT * FROM post WHERE userid=?",
     selectPostByIdx: "SELECT * FROM post WHERE idx = ?", 
     insertPost: "INSERT into post(idx, title, content, writer, postpw, filename, regDate, moDate, count) VALUES(?,?,?,?,?,?,?,?)" ,   
-    deletePostByIdx: "DELETE * FROM post WHERE idx = ?"
+    deletePostByIdx: "DELETE * FROM post WHERE idx = ?",
+    deletePostByIdxPw: "DELETE * FROM post WHERE idx = ? and postpw = ? "
+    
 };
