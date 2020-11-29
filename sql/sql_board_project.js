9d4b0cdd7ad24ffd127e33239277af6037464e29
CultureAddiction <didqhtmd@gmail.com>
2020-11-30 01:34:46 +0900

    mistake:deleteSqlBoards

--- sql/sql_board_project.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    selectPostByUserId: "select * from board_Project where userid=?",
-    insertPost: "insert into board_Project(idx, title, content,writer, filename, regDate, moDate, count) values(?,?,?,?,?,?,?,?)"    
-    // 이게 맞나....
-};
--- sql/sql_board_study.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    selectPostByUserId: "select * from board_Study where userid=?",
-    selectPost: "select * from board_Study where idx =?", 
-    insertPost: "insert into board_Study(idx, title, content,writer, filename, regDate, moDate, count) values(?,?,?,?,?,?,?,?)"    
-};
