657e3b740aa4379f509e62a0e64d8501ca3e469a
sucystem <sucystem@outlook.com>
2020-12-01 00:24:17 +0900

    community

--- sql/board.js
+++ sql/board.js
@@ -1,7 +1,7 @@
 module.exports = {
-    selectPostByPostId: "SELECT p.id, p.title, p.content, p.writer, u.user_name, p.file, p.regDate, p.moDate FROM baeuda.user as u JOIN baeuda.post as p ON p.writer=u.id WHERE p.id=?",
+    selectPostByPostId: "SELECT p.id, p.title, p.content, p.writer, u.user_name, p.file, p.regDate, p.moDate, p.count FROM baeuda.user as u JOIN baeuda.post as p ON p.writer=u.id WHERE p.id=?",
     selectCommentsByPostId: "SELECT c.id, c.content, c.date, c.user_id, u.user_name FROM baeuda.user as u JOIN baeuda.comment as c ON c.user_id=u.id WHERE post_id=? ORDER BY c.date",
-    selectPostsByBoardId: "SELECT * FROM baeuda.post WHERE board_id=?",
+    selectPostsByBoardId: "SELECT p.id, p.title, p.content, p.writer, u.user_name, p.file, p.regDate, p.moDate, p.count FROM baeuda.user as u JOIN baeuda.post as p ON p.writer=u.id WHERE p.board_id=?",
     checkReadLevelByUserId: "SELECT * FROM baeuda.board as b, baeuda.user as u WHERE u.level>=b.read_level AND b.id=? AND u.id=?",
     checkWriteLevelByUserId: "SELECT * FROM baeuda.board as b, baeuda.user as u WHERE u.level>=b.write_level AND b.id=? AND u.id=?",
     insertPost: "INSERT INTO baeuda.post (board_id, title, content, writer, file) VALUES (?, ?, ?, ?, ?)",
