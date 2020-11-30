0e7552b57e0c96f60c7a394a2d1878dc3685c1a0
sucystem <sucystem@outlook.com>
2020-12-01 01:33:07 +0900

    token

--- routes/board.js
+++ routes/board.js
@@ -3,13 +3,16 @@ var router = express.Router();
 var sql = require('../sql')
 var db = require('../modules/db')
 var helper = require('../modules/helper')
+var tokenUser = require('../modules/user');
+
+router.use(tokenUser.tokenToUser);
 
 router.get('/', function (req, res) {
     res.send("board");
 });
 
 router.get('/:board_id', async function (req, res) {
-    const id = JSON.parse(req.headers.user).id;
+    const { id } = req.user._user;
     const { board_id } = req.params;
     try {
         let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
@@ -33,7 +36,7 @@ router.get('/:board_id', async function (req, res) {
 });
 
 router.get('/:board_id/post/:post_id', async function (req, res) {
-    const id = JSON.parse(req.headers.user).id;
+    const { id } = req.user._user;
     const { board_id, post_id } = req.params;
     try {
         let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
@@ -58,7 +61,7 @@ router.get('/:board_id/post/:post_id', async function (req, res) {
 });
 
 router.get('/:board_id/:post_id/comments', async function (req, res) {
-    const id = JSON.parse(req.headers.user).id;
+    const { id } = req.user._user;
     const { board_id, post_id } = req.params;
     try {
         let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
@@ -82,18 +85,20 @@ router.get('/:board_id/:post_id/comments', async function (req, res) {
 });
 
 router.post('/:board_id/newPost', async function (req, res) {
-    const id = JSON.parse(req.headers.user).id;
+    const { id } = req.user._user;
     const { title, content } = req.body;
     const { board_id } = req.params;
     try {
         let [rows] = await db.query(sql.board.checkWriteLevelByUserId, [board_id, id]);
         if (rows.length == 0) {
             res.send({
+                result: "false",
                 msg: "권한이 없습니다."
             })
         } else {
             [rows] = await db.query(sql.board.insertPost, [board_id, title, content, id, null]);
             res.status(200).send({
+                result: "true",
                 postid: rows.insertId,
                 msg: "게시글 등록에 성공했습니다."
             });
@@ -104,7 +109,7 @@ router.post('/:board_id/newPost', async function (req, res) {
 });
 
 router.post('/:board_id/:post_id/addcomment', async function (req, res) {
-    const id = JSON.parse(req.headers.user).id;
+    const { id } = req.user._user;
     const { comment } = req.body;
     const { board_id, post_id } = req.params;
     try {
