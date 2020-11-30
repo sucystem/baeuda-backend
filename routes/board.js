745b82b319d8b230d4c606aecac5659272423de7
sucystem <sucystem@outlook.com>
2020-12-01 00:16:52 +0900

    community

--- routes/board.js
+++ routes/board.js
@@ -9,7 +9,7 @@ router.get('/', function (req, res) {
 });
 
 router.get('/:board_id', async function (req, res) {
-    const { id } = req.user._user[0];
+    const id = JSON.parse(req.headers.user).id;
     const { board_id } = req.params;
     try {
         let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
@@ -32,17 +32,50 @@ router.get('/:board_id', async function (req, res) {
     }
 });
 
-router.get('/post/:post_id', async function (req, res) {
-    const { id } = req.user._user[0];
-    const { post_id } = req.params;
+router.get('/:board_id/post/:post_id', async function (req, res) {
+    const id = JSON.parse(req.headers.user).id;
+    const { board_id, post_id } = req.params;
     try {
-        const [post] = await db.query(sql.board.selectPostByPostId, [post_id]);
-        const [comment] = await db.query(sql.board.selectCommentsByPostId, [post_id]);
-        res.send({
-            result: 'true',
-            data: { post, comment },
-            msg: "게시글 읽기 성공"
-        })
+        let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
+        if (rows.length == 0) {
+            res.send({
+                result: 'false',
+                data: [],
+                msg: "권한이 없습니다."
+            })
+        } else {
+            const [post] = await db.query(sql.board.selectPostByPostId, [post_id]);
+            const [comments] = await db.query(sql.board.selectCommentsByPostId, [post_id]);
+            res.status(200).send({
+                result: 'true',
+                data: { post, comments },
+                msg: "게시글 읽기 성공"
+            })
+        }
+    } catch (e) {
+        helper.failedConnectionServer(res, e);
+    }
+});
+
+router.get('/:board_id/:post_id/comments', async function (req, res) {
+    const id = JSON.parse(req.headers.user).id;
+    const { board_id, post_id } = req.params;
+    try {
+        let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
+        if (rows.length == 0) {
+            res.send({
+                result: 'false',
+                data: [],
+                msg: "권한이 없습니다."
+            })
+        } else {
+            [rows] = await db.query(sql.board.selectCommentsByPostId, [post_id]);
+            res.send({
+                result: 'true',
+                data: rows,
+                msg: "댓글 목록을 읽었습니다."
+            });
+        }
     } catch (e) {
         helper.failedConnectionServer(res, e);
     }
@@ -59,10 +92,32 @@ router.post('/:board_id/newPost', async function (req, res) {
                 msg: "권한이 없습니다."
             })
         } else {
-            await db.query(sql.board.insertPost, [board_id, title, content, id, null]);
+            [rows] = await db.query(sql.board.insertPost, [board_id, title, content, id, null]);
             res.status(200).send({
+                postid: rows.insertId,
                 msg: "게시글 등록에 성공했습니다."
             });
+        }
+    } catch (e) {
+        helper.failedConnectionServer(res, e);
+    }
+});
+
+router.post('/:board_id/:post_id/addcomment', async function (req, res) {
+    const id = JSON.parse(req.headers.user).id;
+    const { comment } = req.body;
+    const { board_id, post_id } = req.params;
+    try {
+        let [rows] = await db.query(sql.board.checkReadLevelByUserId, [board_id, id]);
+        if (rows.length == 0) {
+            res.status(200).send({
+                msg: "권한이 없습니다."
+            })
+        } else {
+            [rows] = await db.query(sql.board.insertComment, [post_id, id, comment]);
+            res.status(200).send({
+                msg: "댓글 등록에 성공했습니다."
+            })
         }
     } catch (e) {
         helper.failedConnectionServer(res, e);
--- sql/board.js
+++ sql/board.js
@@ -1,8 +1,9 @@
 module.exports = {
-    selectPostByPostId: "SELECT * FROM baeuda.post WHERE id=?",
-    selectCommentsByPostId: "SELECT * FROM baeuda.comment WHERE post_id=?",
+    selectPostByPostId: "SELECT p.id, p.title, p.content, p.writer, u.user_name, p.file, p.regDate, p.moDate FROM baeuda.user as u JOIN baeuda.post as p ON p.writer=u.id WHERE p.id=?",
+    selectCommentsByPostId: "SELECT c.id, c.content, c.date, c.user_id, u.user_name FROM baeuda.user as u JOIN baeuda.comment as c ON c.user_id=u.id WHERE post_id=? ORDER BY c.date",
     selectPostsByBoardId: "SELECT * FROM baeuda.post WHERE board_id=?",
     checkReadLevelByUserId: "SELECT * FROM baeuda.board as b, baeuda.user as u WHERE u.level>=b.read_level AND b.id=? AND u.id=?",
     checkWriteLevelByUserId: "SELECT * FROM baeuda.board as b, baeuda.user as u WHERE u.level>=b.write_level AND b.id=? AND u.id=?",
-    insertPost: "INSERT INTO baeuda.post (board_id, title, content, writer, file) VALUES (?, ?, ?, ?, ?)"
+    insertPost: "INSERT INTO baeuda.post (board_id, title, content, writer, file) VALUES (?, ?, ?, ?, ?)",
+    insertComment: "INSERT INTO baeuda.comment (post_id, user_id, content) VALUES (?, ?, ?)"
 }
